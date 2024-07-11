5c2b4767ca3082ceb07a1118ae28dd70a7d03264
Jack Pachon <jack.kilian0196@gmail.com>
2024-07-11 16:15:55 -0500

    changes in vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Mortgage-repayment-calculator/"
+  base:"https://jackp4e5.github.io/Mortgage-repayment-calculator"
 })
