49c25c2a96baa896f4f092962982202ddefd3888
Jack Pachon <jack.kilian0196@gmail.com>
2024-07-15 09:14:13 -0500

    refactoring

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import calculatorImg from "../assets/icon-calculator.svg";
 
 export function Form() {
@@ -17,37 +18,64 @@ export function Form() {
     htmlSpan: "%",
   };
   const mortgage = [amount, term, rate];
+  const [amountField, setAmoutField] = useState("");
+  const [termtField, setTermField] = useState("");
+  const [ratetField, setRateField] = useState("");
+  const [interestFieldInteres, setInterestFieldInteres] = useState("");
+  const [repaymentFieldInteres, setRepaymentFieldInteres] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setAmoutField(e.target[0].value);
+    setTermField(e.target[1].value);
+    setRateField(e.target[2].value);
+    setInterestFieldInteres(e.target[3].value);
+    setRepaymentFieldInteres(e.target[4].id);
+    setTimeout(() => {
+      console.log(
+        amountField,
+        termtField,
+        ratetField,
+        interestFieldInteres,
+        repaymentFieldInteres
+      );
+    }, 500);
+  };
   return (
     <>
-      <form className="form">
+      <form onSubmit={handleSubmit} className="form">
         <div className="input__group">
           {mortgage.map((element, i) => (
             <div className="input__wrapper" key={element.name}>
               <label className={`form__label`} htmlFor={element.name}>
                 {element.name}
               </label>
+              <div className="mask">
                 <input
                   className={`${
                     element.htmlFormLabel === "term" ||
                     element.htmlFormLabel === "rate"
-                      ? "form__input--right-padding"
+                      ? "form__input form__input--right-padding"
                       : "form__input"
                   }`}
                   type="text"
                   id={element.name}
+                  autoComplete="off"
                 />
-              
-              <span
-                className={`${
-                  element.htmlFormLabel === "term"
-                    ? "form__span  form__span--right-term"
-                    : element.htmlFormLabel === "rate"
-                    ? "form__span  form__span--right-rate"
-                    : "form__span"
-                }`}
-              >
-                {element.htmlSpan}
-              </span>
+
+                <span
+                  className={`${
+                    element.htmlFormLabel === "term"
+                      ? "form__span  form__span--right-term"
+                      : element.htmlFormLabel === "rate"
+                      ? "form__span  form__span--right-rate"
+                      : "form__span"
+                  }`}
+                >
+                  {element.htmlSpan}
+                </span>
+              </div>
+              <p className="text_error">hola</p>
             </div>
           ))}
         </div>
