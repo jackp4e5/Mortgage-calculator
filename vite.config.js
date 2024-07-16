26bbfe4a0aa8f14c09eb0b4465aa863b913a39bb
Jack Pachon <jack.kilian0196@gmail.com>
2024-07-16 09:06:26 -0500

    custom domain creation

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://jackp4e5.github.io/Mortgage-repayment-calculator"
+  base:"https://jackpachon.com"
 })
